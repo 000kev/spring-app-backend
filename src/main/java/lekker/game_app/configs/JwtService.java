f70c24d44be943712c272bddb62284b69744ea47
Kevin <Kevin.Kumasamba@anxend.com>
2024-07-16 23:15:48 +0200

    generating/creating a new token

--- src/main/java/lekker/game_app/configs/JwtService.java
+++ src/main/java/lekker/game_app/configs/JwtService.java
@@ -1,18 +1,38 @@
 package lekker.game_app.configs;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "5493BFE632D61695251538F862835";
+
+    public String generateToken(UserDetails userDetails) {
+        return createToken(new HashMap<String, Object>(), userDetails);
+    }
+
+    public String createToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 250 * 60 * 24))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
+    }
     public String extractUsername(String token) {
         return extractClaims(token, Claims::getSubject);
     }
