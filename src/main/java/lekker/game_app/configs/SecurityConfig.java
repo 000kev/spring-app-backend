e599786ea7e2f914250eb64463713ae6e28203d1
Kevin <Kevin.Kumasamba@anxend.com>
2024-07-17 12:11:55 +0200

    adding authentication/login and register controller

--- src/main/java/lekker/game_app/configs/SecurityConfig.java
+++ src/main/java/lekker/game_app/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,26 +20,14 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // http.csrf(csrf -> 
-        // csrf.disable()
-        // .authorizeHttpRequests()
-        // .requestMatchers()
-        // .permitAll()
-        // .anyRequest()
-        // .authenticated()
-        // .and()
-        // .sessionManagement()
-        // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        // .and()
-        // .authenticationProvider(authenticationProvider)
-        // .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class));
+   
         http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(requests -> 
-            requests.requestMatchers("/")
+            requests.requestMatchers("/auth/**")
             .permitAll()
             .anyRequest()
             .authenticated())
-        .sessionManagement(Customizer.withDefaults())
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
---
+++ src/main/java/lekker/game_app/controllers/AuthenticationService.java
@@ -0,0 +1,50 @@
+package lekker.game_app.controllers;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lekker.game_app.configs.JwtService;
+import lekker.game_app.entities.Role;
+import lekker.game_app.entities.User;
+import lekker.game_app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+    
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+            .firstname(request.getFirstname())
+            .lastname(request.getLastname())
+            .email(request.getEmail())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .role(Role.USER)
+            .build();
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        authManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow();
+        
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+    
+}
